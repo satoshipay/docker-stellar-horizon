462f5dfb7562422649f48d6b6f80d641814a0ae1
Ishai Strauss <istrauss@koyadocutracker.com>
2017-08-21 20:15:59 -0500

    Adds some db initialization messages.

--- entry.sh
+++ entry.sh
@@ -2,8 +2,10 @@
 
 { # try
     horizon_init_db
+	echo "finished initializing horizon db"
 } || { # catch
     # if horizon_init_db failed, that means that a db aleady exists which is fine.
+	echo "horizon db already initialized. continuing on..."
 }
 
 horizon_init_db
