b3360b12b1011e05bf57b8a921b0bfe836f438cf
André Gaul <andre@gaul.io>
2018-06-12 09:05:10 +0200

    update to 0.13.0 and use prebuilt binaries

--- install.sh
+++ install.sh
@@ -6,23 +6,14 @@ set -ue
 apt-get update
 apt-get install -y $STELLAR_HORIZON_BUILD_DEPS
 
-# clone, compile, and install stellar core
-git clone --branch $STELLAR_HORIZON_VERSION --recursive --depth 1 https://github.com/stellar/horizon.git
-
-export GOPATH=/var/lib/go
-mkdir -p $GOPATH
-export PATH=$PATH:${GOPATH//://bin:}/bin
-
-cd horizon
-go get github.com/constabulary/gb/...
-gb vendor restore
-gb build
-cp -a bin/* /usr/local/bin/
+# install horizon
+wget https://github.com/stellar/go/releases/download/horizon-v${STELLAR_HORIZON_VERSION}/horizon-v${STELLAR_HORIZON_VERSION}-linux-amd64.tar.gz -O stellar-horizon.tar.gz
+tar -zxvf stellar-horizon.tar.gz
+mv horizon-v${STELLAR_HORIZON_VERSION}-linux-amd64/horizon /usr/local/bin
+chmod +x /usr/local/bin/horizon
 
 # cleanup
-rm -rf horizon
+rm -rf stellar-horizon.tar.gz horizon-v${STELLAR_HORIZON_VERSION}-linux-amd64/
 apt-get remove -y $STELLAR_HORIZON_BUILD_DEPS
 apt-get autoremove -y
-
-# cleanup apt cache
-rm -rf /var/lib/apt/lists/* $GOPATH
+rm -rf /var/lib/apt/lists/*
