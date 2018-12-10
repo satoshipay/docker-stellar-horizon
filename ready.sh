5ec578b9ce528818b268bed64c74953d82eedbe3
André Gaul <andre@gaul.io>
2018-12-10 12:27:59 +0100

    ready: add retry for core

--- ready.sh
+++ ready.sh
@@ -4,7 +4,7 @@ set -eu pipefail
 
 MAX_LEDGER_LAG=10
 
-CORE_INFO=$(curl --silent --max-time 2 ${STELLAR_CORE_URL}/info)
+CORE_INFO=$(curl --silent --max-time 1 --retry 3 --retry-delay 1 ${STELLAR_CORE_URL}/info)
 CORE_STATE=$(jq -r .info.state <<< "${CORE_INFO}")
 if [ "${CORE_STATE}" != "Synced!" ]; then
   >&2 echo "Expected Core state \"Synced!\" but got \"${CORE_STATE}\""
@@ -12,7 +12,7 @@ if [ "${CORE_STATE}" != "Synced!" ]; then
 fi
 
 CORE_LEDGER=$(jq -r .info.ledger.num <<< "${CORE_INFO}")
-HORIZON_LEDGER=$(curl --silent --max-time 2 http://localhost:8000 | jq -r .history_latest_ledger)
+HORIZON_LEDGER=$(curl --silent --max-time 1 http://localhost:8000 | jq -r .history_latest_ledger)
 if (( HORIZON_LEDGER < CORE_LEDGER - MAX_LEDGER_LAG )); then
   >&2 echo "Horizon ledger ${HORIZON_LEDGER} lagging more than ${MAX_LEDGER_LAG} ledgers behind Core ledger ${CORE_LEDGER}"
   exit 1
