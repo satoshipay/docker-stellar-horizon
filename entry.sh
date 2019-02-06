09b8b4d206a1a5f033444765079865a544e28c29
gracenoah <gracenoahgh@gmail.com>
2019-02-06 09:37:38 +0300

    check that db initialization was done properly
    fix #10

--- entry.sh
+++ entry.sh
@@ -2,9 +2,31 @@
 
 set -ue
 
+
 function horizon_init_db() {
   echo "Initializing Horizon database..."
-  horizon db init || echo "Horizon database initialization failed (possibly because it has been done before)"
+  # use /dev/shm so that this works when / is mounted as read only
+  OUT_FILE="$(mktemp -p /dev/shm)"
+  while true; do
+    set +e
+    # capture both the output and the status code because sometimes horizon returns no output and
+    # fails to migrate and other times it returns no output and succeeds and we need the status code
+    # to tell the difference
+    horizon db init > "$OUT_FILE" 2>&1
+    EXIT="$?"
+    set -e
+    if ! [ -s $OUT_FILE ] && [ "$EXIT" = "0" ]; then
+      echo Initialization successful.
+      break
+    elif grep "already exists" "$OUT_FILE" && [ "$EXIT" = "1" ]; then
+      echo Initialization unnecessary.
+      break
+    else
+      echo Initialization failed.
+      cat "$OUT_FILE"
+      sleep 1
+    fi
+  done
 }
 
 if [ ! -z ${DATABASE_URL+x} ] && [ ! -z ${DATABASE_PASSWORD+x} ]; then
